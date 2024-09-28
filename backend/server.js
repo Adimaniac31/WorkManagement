72548d2e6c0229a87b47eec6bfd6052a3427e3b0
adimaniac <adityabhigade31@gmail.com>
2024-09-28 17:26:29 +0530

    removed origin to all

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    // origin: process.env.FRONTEND_URL,
-    origin: '*',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
